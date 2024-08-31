faa72ed3b50d9f72749ea9c6427e3a8088637bf0
Kingddos00 <king00ddos@outlook.com>
2024-08-31 15:21:49 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7247460241:AAGCNZS8unqZ8kXrCAhBhJZGQoB0Gcit08w")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@osama_hacker")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1640964213")
+
+
+
+
+
+
+
+  
